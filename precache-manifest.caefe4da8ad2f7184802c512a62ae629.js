8ef8414cb820f48066af12d0338a55891c97a757
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-05 19:24:07 +0000

    Deploying to gh-pages from  @ 520de0d706bd70c73b2fe87109552f3c2ba9c9d1 🚀

---
+++ precache-manifest.caefe4da8ad2f7184802c512a62ae629.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729d125b2de9994674b4006f2bc42f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad4fd26a45db5fd9740",
+    "url": "/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "7c1fa053d18277b274d9",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "fa4bdb26d60fbefbab80",
+    "url": "/static/css/main.7033e124.chunk.css"
+  },
+  {
+    "revision": "1ff83808d81346eebdc3",
+    "url": "/static/js/2.a79b504d.chunk.js"
+  },
+  {
+    "revision": "9ad4fd26a45db5fd9740",
+    "url": "/static/js/3.f773fc14.chunk.js"
+  },
+  {
+    "revision": "7c1fa053d18277b274d9",
+    "url": "/static/js/4.8401ca8b.chunk.js"
+  },
+  {
+    "revision": "fa4bdb26d60fbefbab80",
+    "url": "/static/js/main.094566dc.chunk.js"
+  },
+  {
+    "revision": "b7c512d24cd9bc42f905",
+    "url": "/static/js/runtime-main.1d3dd508.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2962b0a9a59e31946fac370899d63aab",
+    "url": "/static/media/bhu.2962b0a9.png"
+  },
+  {
+    "revision": "571184df33a9da36409ce12ae1658516",
+    "url": "/static/media/certificate1.571184df.jpg"
+  },
+  {
+    "revision": "5d947fea9d45ce97e3a049be029cde70",
+    "url": "/static/media/certificate2.5d947fea.png"
+  },
+  {
+    "revision": "e9d30b357f9094c900a38457fbf68467",
+    "url": "/static/media/certificate3.e9d30b35.jpg"
+  },
+  {
+    "revision": "4d7663bfcb30decc9a7f1b57d2a79447",
+    "url": "/static/media/certificate4.4d7663bf.jpg"
+  },
+  {
+    "revision": "bab3e761e3d7219e8336c397416b2bb6",
+    "url": "/static/media/codementor.bab3e761.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cabc6a8deb6008b732055a8a1f41f934",
+    "url": "/static/media/iitbhu.cabc6a8d.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "2153f053d36d5de083c1b914ad76541d",
+    "url": "/static/media/tessact.2153f053.png"
+  }
+]);
